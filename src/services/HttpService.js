df47f84242633423706c5fdd79aadee900484510
anuragsr <anurag.131092@gmail.com>
2024-10-23 11:47:41 +0530

    Added mock points using turf.js

--- src/services/HttpService.js
+++ src/services/HttpService.js
@@ -1,9 +1,100 @@
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
-import { l, mock } from "../helpers/common";
+import * as turf from "@turf/turf";
+import { l, mock, rand, randBetween } from "../helpers/common";
 
 let apiHost = "",
   call;
+
+const getRandomRadii = () => {
+    const radius = randBetween(10, 15);
+
+    return {
+      radius,
+      influence_radius: radius + 5
+    };
+  },
+  getTagInfluence = () => {
+    const center = [-73.96625, 40.78343],
+      radius = 0.2,
+      points = 25,
+      randomPoints = turf.randomPoint(points, {
+        bbox: turf.bbox(turf.circle(center, radius))
+      }),
+      response = {
+        id: rand(4),
+        areas: randomPoints.features.map((feature, idx) => {
+          const { coordinates } = feature.geometry,
+            { radius, influence_radius } = getRandomRadii();
+
+          return {
+            id: idx,
+            geometry: {
+              coordinates
+            },
+            properties: {
+              type: "circle",
+              radius
+            },
+            influence_polygon: {
+              properties: {
+                influence_radius
+              },
+              geometry: {
+                coordinates
+              }
+            }
+          };
+        })
+      };
+
+    // l(response);
+    return [response];
+    // Example response
+    // return [
+    //   {
+    //     id: 1,
+    //     areas: [
+    //       {
+    //         id: 1,
+    //         geometry: {
+    //           coordinates: [-73.96625, 40.78343]
+    //         },
+    //         properties: {
+    //           type: "circle",
+    //           radius: 10
+    //         },
+    //         influence_polygon: {
+    //           properties: {
+    //             influence_radius: 15
+    //           },
+    //           geometry: {
+    //             coordinates: [-73.96625, 40.78343]
+    //           }
+    //         }
+    //       },
+    //       {
+    //         id: 2,
+    //         geometry: {
+    //           coordinates: [-73.96625, 40.784877315831444]
+    //         },
+    //         properties: {
+    //           type: "circle",
+    //           radius: 20
+    //         },
+    //         influence_polygon: {
+    //           properties: {
+    //             influence_radius: 25
+    //           },
+    //           geometry: {
+    //             coordinates: [-73.96625, 40.784877315831444]
+    //           }
+    //         }
+    //       }
+    //     ]
+    //   }
+    // ];
+  };
 
 export default class HttpService {
   constructor() {
@@ -31,32 +122,13 @@ export default class HttpService {
         ]
       })
       .onGet("/api/v1/tags-influence")
-      .reply(200, {
-        tags: [
+      .reply(() => {
+        return [
+          200,
           {
-            id: 1,
-            areas: [
-              {
-                id: 1,
-                geometry: {
-                  coordinates: [-73.96625, 40.78343]
-                },
-                properties: {
-                  type: "circle",
-                  radius: 10
-                },
-                influence_polygon: {
-                  properties: {
-                    influence_radius: 15
-                  },
-                  geometry: {
-                    coordinates: [-73.96625, 40.78343]
-                  }
-                }
-              }
-            ]
+            tags: getTagInfluence()
           }
-        ]
+        ];
       });
   }
 
